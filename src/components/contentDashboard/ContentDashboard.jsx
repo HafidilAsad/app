469fd3be55f707f173f5ca95fd1f57bca3687b9a
Hafidil Asad <hafidil.nm03708@aop.oto>
2024-10-10 22:06:41 +0700

    add icon

--- src/components/contentDashboard/ContentDashboard.jsx
+++ src/components/contentDashboard/ContentDashboard.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLightbulb } from '@fortawesome/free-solid-svg-icons';
+import { faLightbulb, faDoorClosed } from '@fortawesome/free-solid-svg-icons';
+import { motion } from "framer-motion";
+import ThreeDotsWave from '../threeDotsWaves';
 
 const doors = ['DOOR 1', 'DOOR 2'];
 const lamps = [
@@ -26,91 +28,136 @@ const kwh = [
 
 ]
 
-const Card = ({ header, body, bodyContent }) => (
-  <div className="card shadow" style={{cursor: "pointer"}}>
+const text = "Control and Monitoring System".split(" ");
+
+const Card = ({ header, bodyContent }) => (
+  <motion.div
+    className="card shadow"
+    style={{ cursor: "pointer" }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 0.5 }}
+  >
     <div className="card-header text-center fw-bold fs-4 rounded-3" style={{ borderBottom: "5px solid #bcbcbc" }}>
       {header}
     </div>
     <div className="card-body text-center">
-      {bodyContent ? bodyContent : <div>{body}</div>}
+      {bodyContent}
     </div>
-  </div>
+  </motion.div>
 );
 
 const ContentDashboard = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <div className='container-fluid'>
-        <h1 className='ms-4 my-3'>Control and Monitoring System</h1>
-      <div className="row my-2 mx-2">
-        <div className="row">
-          <div className="col-lg-6 col-md-12 col-sm-12">
-            <div className="row">
-              {doors.map((door, index) => (
-                <div className="col-lg-6 col-md-6 col-sm-12 mt-3" key={index}>
-                  <Card header={door} body="EMERGENCY STATUS" bodyContent={<div className="fs-1 my-3 text-danger fw-bold">EMERGENCY STATUS</div>} />
+       {loading ? (
+        <ThreeDotsWave />
+      ) : (
+        <>
+              <div className="row my-2 mx-2 me-5">
+                <div className="card shadow-sm mx-2">
+                  <h2 className='ms-4 my-1 p-2'>
+                  {text.map((el, i) => (
+                    <motion.span
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{
+                        duration: 2.55,
+                        delay: i / 10
+                      }}
+                      key={i}
+                    >
+                      {el}{" "}
+                    </motion.span>
+                ))}
+                  </h2>
                 </div>
-              ))}
-            </div>
-            <div className="row mt-4">
-              {lamps.map((lamp, index) => (
-                <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
-                  <Card 
-                    header={lamp.name} 
-                    bodyContent={
-                      <>
-                        <FontAwesomeIcon icon={faLightbulb} size='3x' style={{ color: lamp.color }} />
-                        <br />
-                        LAMP STATUS
-                      </>
-                    } 
-                  />
-                </div>
-              ))}
-            </div>
-            <div className="row mt-4">
-              {switches.map((sw, index) => (
-                <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
-                  <Card 
-                    header={sw.name} 
-                    bodyContent={
-                      <div className={`btn ${sw.btnClass} w-100 fw-bold my-3`} style={{ border: "1px solid #bcbcbc", borderBottom: "5px solid #bcbcbc" }}>
-                        {sw.status}
-                      </div>
-                    } 
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-12 col-sm-12">
-            <div className="row">
-              <div className="col mt-3">
-                <Card header="CCTV"  bodyContent={<div className="fs-1 my-3 fw-bold" style={{minHeight:"11em"}}>DISPLAY CCTV</div>} />
               </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row my-2 mx-2">
-        <div className="row">
-            {metrics.map((metric, index) => (
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
-                    <Card header={metric.name} body={metric.value} bodyContent={<div className="fw-bold fs-1">{metric.value}</div>} />
+              <div className="row my-2 mx-2">
+                <div className="row">
+                  <div className="col-lg-6 col-md-12 col-sm-12">
+                    <div className="row">
+                      {doors.map((door, index) => (
+                        <div className="col-lg-6 col-md-6 col-sm-12 mt-3" key={index}>
+                          <Card header={door} body="EMERGENCY STATUS" bodyContent={<div className="fs-5 my-1 text-danger fw-bold">
+                            <FontAwesomeIcon icon={faDoorClosed} size='2x' />
+                          <br /> Emergency</div>} />
+                        </div>
+                      ))}
+                    </div>
+                    <div className="row mt-4">
+                      {lamps.map((lamp, index) => (
+                        <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
+                          <Card 
+                            header={lamp.name} 
+                            bodyContent={
+                              <>
+                                <FontAwesomeIcon icon={faLightbulb} size='3x' style={{ color: lamp.color }} />
+                                <br />
+                                LAMP STATUS
+                              </>
+                            } 
+                          />
+                        </div>
+                      ))}
+                    </div>
+                    <div className="row mt-4">
+                      {switches.map((sw, index) => (
+                        <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
+                          <Card 
+                            header={sw.name} 
+                            bodyContent={
+                              <div className={`btn ${sw.btnClass} w-100 fw-bold my-3`} style={{ border: "1px solid #bcbcbc", borderBottom: "5px solid #bcbcbc" }}>
+                                {sw.status}
+                              </div>
+                            } 
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-12 col-sm-12">
+                    <div className="row">
+                      <div className="col mt-3">
+                        <Card header="CCTV"  bodyContent={<div className="fs-1 my-3 fw-bold" style={{minHeight:"10.6em"}}>DISPLAY CCTV</div>} />
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            ))}
-        </div>
-       
-      </div>
-      <div className="row my-2 mx-2">
-        <div className="row">
-            {kwh.map((kwh, index) => (
-                <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
-                    <Card header={kwh.name} body={kwh.value} bodyContent={<div className="fw-bold fs-1">{kwh.value}</div>} />
+              </div>
+              <div className="row my-2 mx-2">
+                <div className="row">
+                    {metrics.map((metric, index) => (
+                        <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
+                            <Card header={metric.name} body={metric.value} bodyContent={<div className="fw-bold fs-1">{metric.value}</div>} />
+                        </div>
+                    ))}
                 </div>
-            ))}
-        </div>
-       
-      </div>
+              
+              </div>
+              <div className="row my-2 mx-2">
+                <div className="row">
+                    {kwh.map((kwh, index) => (
+                        <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
+                            <Card header={kwh.name} body={kwh.value} bodyContent={<div className="fw-bold fs-1">{kwh.value}</div>} />
+                        </div>
+                    ))}
+                </div>
+              
+              </div>
+
+        </>
+      )}
+   
     </div>
   );
 };
