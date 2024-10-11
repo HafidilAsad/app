b01f48fa76f7b9473c8cce5e0aaa6450580ee5f3
Hafidil Asad <hafidilaksad@gmail.com>
2024-10-11 21:42:25 +0700

    eror sub domain

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./pages/dashboard/dashboard";
-import Login from "./pages/login/login";
+// import Login from "./pages/login/login";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/" element={<Login />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </div>
