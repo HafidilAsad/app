6eb64a9c74b5a1b0a2b6a7957ade2a6c858b4db6
Hafidil Asad <hafidilaksad@gmail.com>
2024-10-23 22:24:47 +0700

    add card lamp

--- src/components/contentDashboard/ContentDashboard.jsx
+++ src/components/contentDashboard/ContentDashboard.jsx
@@ -1,50 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLightbulb, faDoorClosed } from '@fortawesome/free-solid-svg-icons';
-import { motion } from "framer-motion";
 import ThreeDotsWave from '../threeDotsWaves';
-import ChartTemperature from './ChartTemperature';
 
-
-const doors = ['DOOR 1', 'DOOR 2'];
-const lamps = [
-  { name: 'LAMP 1', color: "#bcbcbc" },
-  { name: 'LAMP 2', color: "#6ff542" }
-];
-const switches = [
-  { name: 'SWITCH 1', status: 'OFF', btnClass: 'btn-light' },
-  { name: 'SWITCH 2', status: 'ON', btnClass: 'btn-success' }
-];
-const metrics = [
-  { name: 'TEMPERATURE 1', value: '29.0' },
-  { name: 'TEMPERATURE 2', value: '27.3' },
-  { name: 'TEMPERATURE 3', value: '25.5' },
-  { name: 'TEMPERATURE 4', value: '17.1' }
-];
-
-const kwh = [
-  { name: 'kWh', value: '273' },
-  { name: 'Voltage', value: '220 V' },
-  { name: 'Current', value: '10 A' },
-  { name: 'Power factor', value: '0.91' }
-
-]
-
-const Card = ({ header, bodyContent }) => (
-  <motion.div
-    className="card shadow-sm border-0 rounded-5"
-    style={{ cursor: "pointer" }}
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 0.5 }}
-  >
-    
-    <div className="card-body text-center " >
-      <div className="fs-5 fw-bold mb-3 text-muted">{header}</div>
-      {bodyContent}
-    </div>
-  </motion.div>
-);
 
 const ContentDashboard = () => {
   const [loading, setLoading] = useState(false);
@@ -64,79 +22,76 @@ const ContentDashboard = () => {
         <ThreeDotsWave />
       ) : (
         <>
-              <div className="row my-2 mx-2">
-                <div className="row">
-                  <div className="col-lg-6 col-md-12 col-sm-12">
-                    <div className="row">
-                      {doors.map((door, index) => (
-                        <div className="col-lg-6 col-md-6 col-sm-12 mt-3" key={index}>
-                          <Card header={door} body="EMERGENCY STATUS" bodyContent={<div className="fs-5 my-1 text-danger fw-bold">
-                            <FontAwesomeIcon icon={faDoorClosed} size='2x' />
-                          <br /> EMERGENCY</div>} />
-                        </div>
-                      ))}
-                    </div>
-                    <div className="row mt-4">
-                      {lamps.map((lamp, index) => (
-                        <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
-                          <Card 
-                            header={lamp.name} 
-                            bodyContent={
-                              <>
-                                <FontAwesomeIcon icon={faLightbulb} size='3x' style={{ color: lamp.color }} />
-                                <br />
-                                LAMP STATUS
-                              </>
-                            } 
+          <div className="row m-2 mt-4">
+            <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+              <div className="row">
+                <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+                  <div className="card shadow rounded-5">
+                    <div className="card-body ">
+                      <ul className='list-unstyled mx-5 d-flex justify-content-between p-4'>
+                        <li>
+                          <FontAwesomeIcon
+                            icon={faLightbulb}
+                            className="me-5"
+                            size="6x"
                           />
-                        </div>
-                      ))}
-                    </div>
-                    <div className="row mt-4">
-                      {switches.map((sw, index) => (
-                        <div className="col-lg-6 col-md-12 col-sm-12" key={index}>
-                          <Card 
-                            header={sw.name} 
-                            bodyContent={
-                              <div className={`btn ${sw.btnClass} w-100 fw-bold my-3`} style={{ border: "1px solid #bcbcbc", borderBottom: "5px solid #bcbcbc" }}>
-                                {sw.status}
-                              </div>
-                            } 
+                          <br />
+                          <div className="form-check form-switch ms-3 my-3">
+                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+                          </div>
+                        </li>
+                        <li>
+                          <FontAwesomeIcon
+                            icon={faLightbulb}
+                            className="me-5"
+                            size="6x"
                           />
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="col-lg-6 col-md-12 col-sm-12">
-                      <div className="col mt-3">
-                        <Card header="CCTV"  bodyContent={<div className="fs-1 my-3 fw-bold" style={{minHeight:"10.6em"}}>DISPLAY CCTV</div>} />
+                            <br />
+                          <div className="form-check form-switch ms-3 my-3">
+                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+                          </div>
+                        </li>
+                        <li>
+                          <FontAwesomeIcon
+                            icon={faLightbulb}
+                            className="me-5"
+                            size="6x"
+                          />
+                            <br />
+                          <div className="form-check form-switch ms-3 my-3">
+                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+                          </div>
+                        </li>
+                        <li>
+                          <FontAwesomeIcon
+                            icon={faLightbulb}
+                            className="me-5"
+                            size="6x"
+                          />
+                            <br />
+                          <div className="form-check form-switch ms-3 my-3">
+                            <input className="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked />
+                          </div>
+                        </li>
+                       
+                      </ul>
+                      <div className="card mx-5 text-center">
+                        <h3 className='p-4'>
+                            ENERGY CONSUMPTION : 100W
+                        </h3>
                       </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row my-2 mx-2">
-                <div className="row">
-                    {metrics.map((metric, index) => (
-                        <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
-                            <Card header={metric.name} body={metric.value} bodyContent={<div className="fw-bold fs-1">
-                              <ChartTemperature valueTemperature={metric.value}/>
-                            </div>} />
-                        </div>
-                    ))}
+                <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                  ini chart
                 </div>
-              
               </div>
-              <div className="row my-2 mx-2">
-                <div className="row">
-                    {kwh.map((kwh, index) => (
-                        <div className="col-lg-3 col-md-6 col-sm-12 mt-3" key={index}>
-                            <Card header={kwh.name} body={kwh.value} bodyContent={<div className="fw-bold fs-1">{kwh.value}</div>} />
-                        </div>
-                    ))}
-                </div>
-              
-              </div>
-
+            </div>
+            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+              ini cctv
+            </div>
+          </div>
         </>
       )}
    
